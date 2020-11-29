49e504baabbc2fd6f42fa187ca513b97d6ac7df2
Emmanuelle Roberge <emmanuelle.roberge@polymtl.ca>
2020-11-29 16:28:45 -0500

    ER : All bugs are fixed

--- TP3/Source/SoftDesignTraining/Task/MoveToPlayer.cpp
+++ TP3/Source/SoftDesignTraining/Task/MoveToPlayer.cpp
@@ -3,18 +3,10 @@
 
 #include "MoveToPlayer.h"
 #include "../SDTAIController.h"
-#include "DrawDebugHelpers.h"
-
-#include "BehaviorTree/Blackboard/BlackboardKeyType_Bool.h"
-#include "BehaviorTree/Blackboard/BlackboardKeyType_Vector.h"
-#include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
-#include "Blueprint/AIBlueprintHelperLibrary.h"
 
 EBTNodeResult::Type UMoveToPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
 	if (ASDTAIController* aiController = Cast<ASDTAIController>(OwnerComp.GetAIOwner())) {
 		aiController->MoveToPlayer();
-
-		DrawDebugSphere(GetWorld(), aiController->GetPawnLocation() + FVector(0.f, 0.f, 100.f), 20.f, 32, FColor::Green, false, 0.035f);
 
 		return EBTNodeResult::Succeeded;
 	}
